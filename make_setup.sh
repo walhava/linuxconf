a46aa9babfd794434d51406386817a84344c79d2
Ville Kankainen <ville.kankainen@symbio.com>
2019-04-25 10:05:52 +0300

    updated the if :P

--- make_setup.sh
+++ make_setup.sh
@@ -4,7 +4,7 @@ LINUXCONF="$HOME/.linuxconf"
 
 
 # check if directory exists
-if [ -d $LINUXCONF ]; then
+if [ ! -d "$LINUXCONF" ]; then
 	echo cloning github.com/walhava/linuxconf to $LINUXCONF
 	git clone git@github.com:walhava/linuxconf.git  $LINUXCONF
 	cd $LINUXCONF
