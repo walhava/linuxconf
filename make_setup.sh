187aacbf01c030d3ebdca894e4e17dbb632921bd
Ville Kankainen <ville.kankainen@gmail.com>
2019-10-21 10:55:47 +0300

    added necessary install dependencies

--- make_setup.sh
+++ make_setup.sh
@@ -5,6 +5,18 @@ LINUXCONF="$HOME/.linuxconf"
 
 # check if directory exists
 if [ ! -d "$LINUXCONF" ]; then
+	sudo apt update
+	sudo apt install --yes \
+		vim \
+		git \
+		build-essential \
+		tmux \
+		zsh
+
+	sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+	chsh -s $(which zsh)
+
 	echo cloning github.com/walhava/linuxconf to $LINUXCONF
 	git clone git@github.com:walhava/linuxconf.git  $LINUXCONF
 	cd $LINUXCONF
